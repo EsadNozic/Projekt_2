f47d94fa2e426b853a174fe1278d4a38b3e70bf0
EsadNozic <esad.nozic@fba.ba>
2022-02-13 17:20:16 +0100

    Promjene

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,13 @@ public class Main {
 	// write your code here
         int i;
         int j;
+        int k;
+        i=1;
         System.out.println ("Hello world 1");
         System.out.println ("Hello world 2");
         System.out.println("Hello world 3");
+        System.out.println ("Varijabla i : " + i );
+
 
 
     }
