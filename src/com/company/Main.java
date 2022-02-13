63d8ac71451c5cc3f3202a43d65fcace539bcfc8
EsadNozic <esad.nozic@fba.ba>
2022-02-13 14:29:36 +0100

    Promjene

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int i;
         System.out.println ("Hello world 1");
         System.out.println ("Hello world 2");
         System.out.println("Hello world 3");
+
+
     }
 }
